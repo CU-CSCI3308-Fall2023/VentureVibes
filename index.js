613c776153a73dcaab8dd27fd7dc772ab7370091
maechen <mchenco7@gmail.com>
2023-11-14 23:19:25 -0700

    Merging index.js from main

--- index.js
+++ index.js
@@ -64,56 +64,34 @@ app.get('/mytrips', (req, res) => {
   res.render('pages/mytrips');
 });
 app.get('/discover', (req, res) => {
-  res.render('pages/discover');
+  var test = null;
+  res.render('pages/discover', {test,});
 });
-
-app.post("/login", async (req, res) => {
-    try {
-        const username = req.body.username;
-
-        if (/^\d+$/.test(username)) {
-          res.json({ status: "success", message: "Invalid input" });
-          return res.render("pages/login", {
-              message: "Invalid Username",
-          });
-        }
-        const userData = await db.query(
-            "SELECT * FROM users WHERE username = $1",
-            [username]
-        );
-
-        if (userData.length === 0) {
-            // If the user is not found, redirect to GET /register route
-            return res.redirect("/register");
-        }
-
-        const user = userData[0];
-
-        if (req.body.password != user.password) {
-          res.json({ status: "success", message: "Success" });
-            // If the password is incorrect, throw an error
-            return res.render("pages/login", {
-                message: "Invalid Input",
-            });
-        }
-
-        // Set the session for the logged-in user
-        req.session.user = user;
-        req.session.save();
-        res.json({ status: "success", message: "Success" });
-        // Redirect to /discover route after setting the session
-        res.redirect("/discover");
-    } catch (error) {
-        // If the database request fails, send an appropriate message to the user and render the login.ejs page
-        res.json({ status: "success", message: "Success" });
-        res.render("pages/login", {
-            message: "Invalid Input",
-        });
-    }
-});
-
-app.get("/welcome", (req, res) => {
-    res.json({ status: "success", message: "Welcome!" });
+app.get('/discoverData', async (req,res) => {
+  axios({
+    url: `https://api.content.tripadvisor.com/api/v1/location/nearby_search?language=en`,
+    method: 'GET',
+    dataType: 'json',
+    headers: {
+      'Accept-Encoding': 'application/json',
+    },
+    params: {
+      key: process.env.ADVISOR_KEY,
+      latLong: `${req.query.latitude},${req.query.longitude}`, //you can choose any artist/event here
+      radius: '10',
+      radiusUnit: 'mi'
+    },
+  })
+    .then(results => {
+      console.log(req.query.latitude); // the results will be displayed on the terminal if the docker containers are running // Send some parameters
+      console.log(req.query.longitude);
+      var test = results.data;
+      return res.render("pages/discover", {test,});
+    })
+    .catch(error => {
+      // Handle errors
+      console.log(error);
+    });
 });
 
 // Authentication Middleware.
@@ -130,6 +108,5 @@ app.use(auth);
 // <!-- Section 5 : Start Server-->
 // *****************************************************
 // starting the server and keeping the connection open to listen for more requests
-// app.listen(3000);
-module.exports = app.listen(3000);
+app.listen(3000);
 console.log('Server is listening on port 3000');
